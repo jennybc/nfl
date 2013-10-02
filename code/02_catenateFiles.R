f61d97944e5488c7500b5216ba52c36b052db014
jennybc <jenny@stat.ubc.ca>
2013-10-02 16:50:24 -0700

    Refactoring the pre-merge dataset harmonization code

--- code/02_catenateFiles.R
+++ code/02_catenateFiles.R
@@ -10,7 +10,7 @@ if(length(jFiles) != 21)
   stop("expecting 21 raw data files!\ncheck what's gone wrong!")
 
 ## http://stackoverflow.com/questions/4993837/r-invalid-multibyte-string
-## helped me troubleshoot this error upon importing a single dataset:
+## helped me troubleshoot this error upon importing a single test dataset:
 ## Error in type.convert(data[[i]], as.is = as.is[i], dec = dec, na.strings = character(0L)) : 
 ##   invalid multibyte string at '<a0>'
 
@@ -23,21 +23,23 @@ str(allRaw, max.level = 1)
 ## OMG they all have different numbers of variables
 
 ## assemble number of observations and variables for each dataset
-dsetFacts <- ldply(allRaw, function(x) {
-  data.frame(nobs = nrow(x), nvar = length(x))
-})
-dsetFacts <- data.frame(do.call("rbind", strsplit(dsetFacts$.id, "_")),
-                        dsetFacts)
-names(dsetFacts) <- c("year", "position", "ID", "nobs", "nvar")
-dsetFacts$year <- as.numeric(as.character(dsetFacts$year))
-str(dsetFacts)
+getDsetFacts <- function(z) {
+  dsetFacts <-
+    ldply(z, function(x) data.frame(nobs = nrow(x), nvar = length(x)))
+  dsetFacts <-
+    data.frame(do.call("rbind", strsplit(dsetFacts$.id, "_")), dsetFacts)
+  names(dsetFacts) <- c("year", "position", "ID", "nobs", "nvar")
+  dsetFacts$year <- as.numeric(as.character(dsetFacts$year))
+  dsetFacts
+}
+dsetFacts <- getDsetFacts(allRaw)
 dsetFacts
 
 ## is the number of variables at least constant within a position?
-dlply(dsetFacts, ~ position, function(x) sort(x$nvar))
+dlply(dsetFacts, ~ position, function(x) x$nvar)
 ## no, of course not, that would be too easy
 ## good ones: DEF = 13, QB = 21, ST = 8
-## tricky ones: K = 21, 21, 26; RB = 18, 19, 19; TE = 18, 19, 19; WR = 23, 24, 24
+## tricky ones: K = 26, 21, 21; RB = 19, 18, 19; TE = 19, 18, 19; WR = 24, 23, 24
 
 ## which variables appear, by position?
 
@@ -50,12 +52,12 @@ llply(allRaw, names)
 
 ## visual inspection of the tricky ones
 
-## K = 21, 21, 26
+## K = 26, 21, 21
 ## in 2010, we have field goals in 5 different length classes missed? made? attempted?
 ## in 2011 and 2012, we have only made and attempted?
 ## that explains the extra 5 variables in 2010
 
-## RB = 18, 19, 19 and TE = 18, 19, 19 and WR = 23, 24, 24
+## RB = 19, 18, 19 and TE = 19, 18, 19 and WR = 24, 23, 24
 ## 2010 has Rec.Tgt
 ## 2011 had nothing
 ## 2012 has Targets
@@ -68,12 +70,7 @@ allRaw[["2010_K"]] <-
          select = !grepl("^X[0-9]", names(allRaw[["2010_K"]])))
 
 ## in 2010_RB, 2010_TE, 2010_WR, rename Rec.Tgt to Targets
-jFun <- function(x) {
-  theNames <- names(x)
-  theNames[theNames == "Rec.Tgt"] <- "Targets"
-  names(x) <- theNames
-  x
-}
+jFun <- function(x) rename(x, c(Rec.Tgt = "Targets"))
 allRaw[["2010_RB"]] <- jFun(allRaw[["2010_RB"]])
 allRaw[["2010_TE"]] <- jFun(allRaw[["2010_TE"]])
 allRaw[["2010_WR"]] <- jFun(allRaw[["2010_WR"]])
@@ -88,17 +85,9 @@ allRaw[["2011_TE"]] <- jFun(allRaw[["2011_TE"]])
 allRaw[["2011_WR"]] <- jFun(allRaw[["2011_WR"]])
 
 ## let's count the variables again ...
-dsetFacts <- ldply(allRaw, function(x) {
-  data.frame(nobs = nrow(x), nvar = length(x))
-})
-dsetFacts <- data.frame(do.call("rbind", strsplit(dsetFacts$.id, "_")),
-                        dsetFacts)
-names(dsetFacts) <- c("year", "position", "ID", "nobs", "nvar")
-dsetFacts$year <- as.numeric(as.character(dsetFacts$year))
-str(dsetFacts)
+dsetFacts <- getDsetFacts(allRaw)
 dsetFacts
 dlply(dsetFacts, ~ position, function(x) sort(x$nvar))
-
 ## YES! within position, the number of variables is now constant
 
 ## but do they really have the same names?
@@ -113,8 +102,8 @@ lapply(jPosition, function(x) {
   rownames(foo) <- NULL
   with(foo, table(varName, ID))  
 })
-## tells me our only trouble is with WR
-## retained bits that show the problem:
+## our only troublesome position is WR
+## retained bits that show the problems:
 #           ID
 # varName    2010_WR 2011_WR 2012_WR
 # KR.Lng         1       0       0
