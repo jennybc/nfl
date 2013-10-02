841f82d8ff1f43101d8f54317c459170ea78a844
jennybc <jenny@stat.ubc.ca>
2013-10-02 16:22:36 -0700

    Standardize the number of variables within the datasets for a single position

--- code/02_catenateFiles.R
+++ code/02_catenateFiles.R
@@ -1,17 +1,18 @@
+## bring the individual datasets into one big file
+## NOTE FROM THE FUTURE: probably not going to happen
+## compromise: merge the year-specific datsets into one dataset per position
+
 library(plyr)
 
-jFiles <- list.files("data", "^201[0-9]_[A-Z]+.csv", full.names = TRUE)
+(jFiles <- list.files("data", "^201[0-9]_[A-Z]+.csv", full.names = TRUE))
 
 if(length(jFiles) != 21) 
   stop("expecting 21 raw data files!\ncheck what's gone wrong!")
-
 
 ## http://stackoverflow.com/questions/4993837/r-invalid-multibyte-string
 ## helped me troubleshoot this error upon importing a single dataset:
 ## Error in type.convert(data[[i]], as.is = as.is[i], dec = dec, na.strings = character(0L)) : 
 ##   invalid multibyte string at '<a0>'
-
-read.table(jFiles[2], header = TRUE, sep = ",", fileEncoding="latin1", quote = )
 
 allRaw <- lapply(jFiles, 
                  function(aFile)
@@ -26,8 +27,10 @@ dsetFacts <- ldply(allRaw, function(x) {
   data.frame(nobs = nrow(x), nvar = length(x))
 })
 dsetFacts <- data.frame(do.call("rbind", strsplit(dsetFacts$.id, "_")),
-                        subset(dsetFacts, select = c(nobs, nvar)))
-names(dsetFacts) <- c("year", "position", "nobs", "nvar")
+                        dsetFacts)
+names(dsetFacts) <- c("year", "position", "ID", "nobs", "nvar")
+dsetFacts$year <- as.numeric(as.character(dsetFacts$year))
+str(dsetFacts)
 dsetFacts
 
 ## is the number of variables at least constant within a position?
@@ -35,6 +38,70 @@ dlply(dsetFacts, ~ position, function(x) sort(x$nvar))
 ## no, of course not, that would be too easy
 ## good ones: DEF = 13, QB = 21, ST = 8
 ## tricky ones: K = 21, 21, 26; RB = 18, 19, 19; TE = 18, 19, 19; WR = 23, 24, 24
+
+## which variables appear, by position?
+
+## first, rearrange dsetFacts and allRaw by position, then year
+newOrder <- with(dsetFacts, order(position, year))
+dsetFacts <- dsetFacts[newOrder, ]
+allRaw <- allRaw[dsetFacts$ID]
+
+llply(allRaw, names)
+
+## visual inspection of the tricky ones
+
+## K = 21, 21, 26
+## in 2010, we have field goals in 5 different length classes missed? made? attempted?
+## in 2011 and 2012, we have only made and attempted?
+## that explains the extra 5 variables in 2010
+
+## RB = 18, 19, 19 and TE = 18, 19, 19 and WR = 23, 24, 24
+## 2010 has Rec.Tgt
+## 2011 had nothing
+## 2012 has Targets
+
+## fix individual datasets to facilitate merging
+
+## in 2010_K, drop variables that start with X[0-9], should be 5 of them
+allRaw[["2010_K"]] <- 
+  subset(allRaw[["2010_K"]],
+         select = !grepl("^X[0-9]", names(allRaw[["2010_K"]])))
+
+## in 2010_RB, 2010_TE, 2010_WR, rename Rec.Tgt to Targets
+jFun <- function(x) {
+  theNames <- names(x)
+  theNames[theNames == "Rec.Tgt"] <- "Targets"
+  names(x) <- theNames
+  x
+}
+allRaw[["2010_RB"]] <- jFun(allRaw[["2010_RB"]])
+allRaw[["2010_TE"]] <- jFun(allRaw[["2010_TE"]])
+allRaw[["2010_WR"]] <- jFun(allRaw[["2010_WR"]])
+
+## in 2011_RB, 2011_RB, 2011_WR, create a variable Targets, all values NA
+jFun <- function(x) {
+  x$Targets <- rep(NA_integer_, nrow(x))
+  x
+}
+allRaw[["2011_RB"]] <- jFun(allRaw[["2011_RB"]])
+allRaw[["2011_TE"]] <- jFun(allRaw[["2011_TE"]])
+allRaw[["2011_WR"]] <- jFun(allRaw[["2011_WR"]])
+
+## let's count the variables again ...
+dsetFacts <- ldply(allRaw, function(x) {
+  data.frame(nobs = nrow(x), nvar = length(x))
+})
+dsetFacts <- data.frame(do.call("rbind", strsplit(dsetFacts$.id, "_")),
+                        dsetFacts)
+names(dsetFacts) <- c("year", "position", "ID", "nobs", "nvar")
+dsetFacts$year <- as.numeric(as.character(dsetFacts$year))
+str(dsetFacts)
+dsetFacts
+dlply(dsetFacts, ~ position, function(x) sort(x$nvar))
+
+## YES! within position, the number of variables is now constant
+
+## but do they reall have the same names?
 
 ## WHEN I RETURN:
 ## ANALYZE VARIABLE NAMES WITHIN POSITION
