c95e99707a4391d392868bf0516b2ebd887b7d79
jennybc <jenny@stat.ubc.ca>
2013-10-02 17:50:49 -0700

    Push downloaded files down into a subdirectory

--- code/01_renameFiles.R
+++ code/01_renameFiles.R
@@ -8,19 +8,19 @@
 ## this script captures my manual process
 
 ## manually download the 2012 files with the mouse
-(jFiles <- list.files("data"))
-file.rename(from = file.path("data", jFiles),
-            to = file.path("data", paste0("2012_",jFiles)))
+(jFiles <- list.files("data/asDownloaded"))
+file.rename(from = file.path("data/asDownloaded", jFiles),
+            to = file.path("data/asDownloaded", paste0("2012_",jFiles)))
 
 ## manually download the 2011 files
-(jFiles <- list.files("data", "^[a-zA-z]"))
-file.rename(from = file.path("data", jFiles),
-            to = file.path("data", paste0("2011_",jFiles)))
+(jFiles <- list.files("data/asDownloaded", "^[a-zA-z]"))
+file.rename(from = file.path("data/asDownloaded", jFiles),
+            to = file.path("data/asDownloaded", paste0("2011_",jFiles)))
 
 ## manually download the 2010 files
-(jFiles <- list.files("data", "^[a-zA-z]"))
-file.rename(from = file.path("data", jFiles),
-            to = file.path("data", paste0("2010_",jFiles)))
+(jFiles <- list.files("data/asDownloaded", "^[a-zA-z]"))
+file.rename(from = file.path("data/asDownloaded", jFiles),
+            to = file.path("data/asDownloaded", paste0("2010_",jFiles)))
 
 list.files("data") # what a mess!
 # [1] "data/2010_DEF.csv"     "data/2010_K.csv"       "data/2010_QB.csv.txt" 
@@ -32,23 +32,23 @@ list.files("data") # what a mess!
 # [19] "data/2012_ST.csv.txt"  "data/2012_TE.csv.txt"  "data/2012_WR.csv.txt" 
 
 ## get rid of gratuitous ".txt" in filename
-(jFiles <- list.files("data", full.names = TRUE))
+(jFiles <- list.files("data/asDownloaded", full.names = TRUE))
 file.rename(from = jFiles, to = gsub("(*.csv).txt", "\\1", jFiles))
 
-list.files("data") # aaaand, of course, the capitalization is not consistent
+list.files("data/asDownloaded") # aaaand, of course, the capitalization is not consistent
 # [1] "2010_DEF.csv" "2010_K.csv"   "2010_QB.csv"  "2010_RB.csv"  "2010_ST.csv"  "2010_TE.csv" 
 # [7] "2010_WR.csv"  "2011_Def.csv" "2011_K.csv"   "2011_QB.csv"  "2011_RB.csv"  "2011_ST.csv" 
 # [13] "2011_TE.csv"  "2011_WR.csv"  "2012_DEF.csv" "2012_K.csv"   "2012_QB.csv"  "2012_RB.csv" 
 # [19] "2012_ST.csv"  "2012_TE.csv"  "2012_WR.csv"
 
 ## GO ALL CAPS for the positions
-(jFiles <- list.files("data"))
-file.rename(from = file.path("data", jFiles),
-            to = file.path("data",
+(jFiles <- list.files("data/asDownloaded"))
+file.rename(from = file.path("data/asDownloaded", jFiles),
+            to = file.path("data/asDownloaded",
                            gsub("(_[a-zA-Z]+)", "\\U\\1", jFiles, perl = TRUE)))
 
 ## THIS is how they should have been named in the first place!
-matrix(list.files("data"), ncol = 3)
+matrix(list.files("data/asDownloaded"), ncol = 3)
 # [,1]           [,2]           [,3]          
 # [1,] "2010_DEF.csv" "2011_DEF.csv" "2012_DEF.csv"
 # [2,] "2010_K.csv"   "2011_K.csv"   "2012_K.csv"  
